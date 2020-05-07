3abb0eaa5eb0d6ab33fbba9bd436fa4b6695d5be
LYH <450446766@qq.com>
2020-05-07 16:51:53 +0800

    测试

--- app/src/main/java/com/hongling/schooltimetable/CourseModel.java
+++ app/src/main/java/com/hongling/schooltimetable/CourseModel.java
@@ -6,7 +6,6 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
